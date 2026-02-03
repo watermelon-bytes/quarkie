5e9c386f38f8eff4ec2831833f6c9a667f59d9f8
watermelon-bytes <water@melons.io>
2026-02-03 22:10:06 +0200

    Made `external_interface` to be a global filesystem variable. Corrected something and added more comments. Now instead of calling interface member field, we will make use of the global instance of `struct low_level_interface`. But also, if we have more than one superblock, than what if they use different disk interfaces?! But already better than just member of `quarkie::superblock`. At least because after rebooting those API functions pointers stored in superblock do not make any sense.

--- inc/superblock.hxx
+++ inc/superblock.hxx
@@ -1,7 +1,7 @@
 #ifndef SUPERBLOCK_HXX
 #define SUPERBLOCK_HXX
 #include <common_api.hxx>
-#include <quarkie_defs.hxx>
+#include <free_space_tracker.hxx>
 
 namespace quarkie {
 
@@ -10,7 +10,6 @@ struct superblock {
     u32 signature;
     /**< A kind of FS identificator */
     // static int8_t sector_size;
-    u16 block_size;
     sector_no total_blocks;
 
     uint global_node_counter {1};
@@ -21,21 +20,19 @@ struct superblock {
 
     // #auxilary
     constexpr inline uint blocks_needed_for(size_t bytes_count) const {
-        return div_and_ceil(bytes_count, block_size);
+        return div_and_ceil(bytes_count, sector_size);
     }
 
     /* NOTE: This allocator should be used to a sort of dynamically manage nodes
      * and must be the only way how nodes are created. */
     constexpr static uint nodes_limit = 1000;
 
-    disk_address root;
-
-    const low_level_interface*
-        external_interface; /**< The whole interface used by filesystem. Pointer
-                               is used for conviniency. */
+    disk_address root_node;
+    free_space::global_metadata global_metadata_;
 
     explicit superblock(const sector_no given_space,
                         const low_level_interface*);
+    superblock() {}
 
     node* get_node_by_path(const char*) const;
 
@@ -45,20 +42,18 @@ struct superblock {
 // Used to describe currently open files
 struct file_entry {
     node* fdescriptor_node;
-    uint8_t access_mode;
-    union {
-        /* For a file. Current r/w offset position */
-        uint cursor;
-
-        /* For a directory (i.e. for `quarkie::read_dir`) */
-        node* next_item_to_read;
-    };
+    u8 access_mode : 7;
+    u8 dir_flag : 1;
+    uint cursor;
 };
 
-constexpr static uint max_open_files = 50;
+constexpr uint max_open_files = 50;
 extern quarkie::pool<file_entry, max_open_files> open_files_table;
+low_level_interface
+    external_interface; /**< The whole interface used by filesystem. Pointer
+                           is used for conviniency. */
 
-extern superblock sb;
+superblock sb;
 
 }  // namespace quarkie
 
